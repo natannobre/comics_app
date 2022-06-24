4ff81847f5e117ba2a0b52e34a5092fa0428f053
natannobre <natannobre37@gmail.com>
2022-06-23 23:54:56 -0300

    Map root page; add controller and view

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    @title = "Marvel Comics"
+    @image_src = "https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&w=1600"
+  end
+end
