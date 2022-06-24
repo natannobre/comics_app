cc17282d1dce6692814cb0986cc259d0b07cf7b8
natannobre <natannobre37@gmail.com>
2022-06-24 02:41:45 -0300

    Faker integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,34 @@
+require './app/api/marvel/comic.rb'
+
 class HomeController < ApplicationController
+  before_action :list_all_comics, only: [:index]
+
   def index
     @title = "Marvel Comics"
-    @image_src = "https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&w=1600"
   end
+
+  private
+    def list_all_comics
+      @comics = Marvel::Comic.fetch_list_of_comics
+
+      @comics_infos = []
+      @comics[:data][:results].each do |comic|
+        @comics_infos << {
+                          path: build_image_path(comic),
+                          title: comic[:title],
+                          date: comic[:dates][0][:date]
+                        }
+      end
+
+      @comics_infos
+    end
+
+    def build_image_path(comic)
+      path = comic[:thumbnail][:path] # || comic[:images][0][:path]
+      variant_name = "portrait_uncanny" # 300x450px
+      extension = comic[:thumbnail][:extension]
+
+      "#{path}/#{variant_name}.#{extension}"
+    end
+
 end
