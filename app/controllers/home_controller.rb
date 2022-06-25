c5e839fa7d175081fb48cfd6ad38739d42a7148f
natannobre <natannobre37@gmail.com>
2022-06-25 20:06:29 -0300

    add character entity request to api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,13 @@ class HomeController < ApplicationController
   before_action :check_page, only: [:index]
 
   def index
-    @comics = Comic.find_per_page(page: params[:page], per_page: @comics_per_page)
+    @comics_per_page = 25
+
+    if session[:character_id].nil?
+      @comics = Comic.all(page: params[:page], per_page: @comics_per_page)
+    else
+      @comics = Comic.find_per_character(character: session[:character_id], page: params[:page], per_page: @comics_per_page)
+    end
   end
 
   def next_page
@@ -17,11 +23,19 @@ class HomeController < ApplicationController
     redirect_to root_path(params[:page])
   end
 
+  def search_character
+    session[:character_id] = nil
+    if params[:name].present?
+      character = Character.find_by_name(params[:name])
+      session[:character_id] = character[:id] if character.present?
+    end
+    redirect_to root_path
+  end
+
   private
 
   def check_page
-    @comics_per_page = 25
-    return unless params[:page].nil? || params[:page].to_i <= 0 || params[:page].to_i > (52_361 / @comics_per_page).to_i
+    return unless params[:page].nil? || params[:page].to_i <= 0
 
     params[:page] = 1 # default page
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   get '/(:page)', to: 'home#index', as: :root
 
+  # Pagination
   post '/(:page)/next_page', to: 'home#next_page', as: :next_page
   post '/(:page)/prev_page', to: 'home#prev_page', as: :prev_page
+
+  post 'search_character/(:name)', to: 'home#search_character', as: :search_character
 end
