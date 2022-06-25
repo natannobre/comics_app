6958803acd6d759172e451bc0f2e32b1e01dfde7
natannobre <natannobre37@gmail.com>
2022-06-25 16:39:28 -0300

    refactor layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  get '/(:page)', to: 'home#index', as: :root
+
+  post '/(:page)/next_page', to: 'home#next_page', as: :next_page
+  post '/(:page)/prev_page', to: 'home#prev_page', as: :prev_page
 end
