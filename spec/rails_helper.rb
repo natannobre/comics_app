253a5df9e164e1ff8fa02babcec6271a8b59afd5
natannobre <natannobre37@gmail.com>
2022-06-30 21:44:29 -0300

    add factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,8 +33,12 @@ end
 RSpec.configure do |config|
 
   # Devise
+  config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
